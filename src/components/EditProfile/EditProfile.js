a8c88c43705df7fb0da22f33946609878765ea3a
dhruval chatapp2 <dst@narola.email>
2021-08-05 14:09:45 +0530

    working on update records 5aug

--- src/components/EditProfile/EditProfile.js
+++ src/components/EditProfile/EditProfile.js
@@ -3,70 +3,49 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { useHistory, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useFormik } from "formik";
-import {getSingleUser} from "../../redux/action/userProfileAction"
+import {
+  getUserUpdatedProfile,
+  getUserProfile,
+} from "../../redux/action/editUserProfileActions";
+
 const EditProfile = () => {
 
-  const {id} = useParams();
+  const data = useSelector((state) => state.editProfile);
+  const { userProfileData } = data;
+
+  const { id } = useParams();
   const dispatch = useDispatch();
   const [showAlert, setShowAlert] = useState(false);
+  const [Name, setName] = useState("");
   const history = useHistory();
 
   useEffect(() => {
-    console.log('hi');
-    dispatch(getSingleUser(id));
-  },[dispatch]);
+    dispatch(getUserProfile(id));
+  }, [dispatch, id]);
 
-  const data = useSelector((state) => state.singleUser);
-  const {userData} = data;
-  console.log(userData);
+  // const data = useSelector((state) => state.editProfile);
+  // const { userProfileData } = data;
+  console.log(userProfileData);
 
-  const formik = useFormik({
-    initialValues: {
-      name: userData.name,
-      // email: userData.email,
-      // Address: {
-      //   city: userData.city,
-      //   state: userData.state,
-      //   country: userData.country,
-      // },
-      // background: userData.background,
-      // ideatostart: userData.ideatostart,
-      // Themes: [],
-      // Skills: [],
-      // Expertise: [],
-    },
+  const onSubmit = () => {
+    let dataEdited = {
+      name: Name,
+      email: "",
+    };
+    console.log(dataEdited);
+    dispatch(getUserUpdatedProfile(dataEdited, id));
+    history.push("/");
+  };
 
-    onSubmit: (values) => {
-      console.log(values);
-
-      let dataEdited = {
-        name: values.name,
-        // email: values.email,
-        // Address: {
-        //   city: values.city,
-        //   state: values.state,
-        //   country: values.country,
-        // },
-        // background: values.background,
-        // ideatostart: values.ideatostart,
-        // Themes: [],
-        // Skills: [],
-        // Expertise: [],
-      };
-      dispatch(getSingleUser(dataEdited, id));
-      history.push("/");
-    },
-  });
-
+  
   return (
     <div className="container pt-3" style={{ maxWidth: "540px" }}>
       <div className="row justify-content-center p-3">
         <div className="col-md-12">
           <h1 className="pb-4">Update Profile Details</h1>
-          <form onSubmit={formik.handleSubmit}>
+          <form onSubmit={onSubmit}>
             <div className="row">
-            <div className="col-md-4 col-sm-4 col-xs-4">
+              <div className="col-md-4 col-sm-4 col-xs-4">
                 <label
                   htmlFor="name"
                   className="mt-2"
@@ -84,7 +63,8 @@ const EditProfile = () => {
                   variant="filled"
                   size="small"
                   type="text"
-                  value={formik.values.name}
+                  value={Name || userProfileData.name}
+                  onChange={(event) => setName(event.target.value)}
                 />
               </div>
             </div>
